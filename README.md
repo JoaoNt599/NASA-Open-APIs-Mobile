7e311f9f5963406fce10bbb07e9f3892ea740b4f
João Vieira <netojoao599@gmail.com>
2024-09-29 21:09:58 -0300

    New design and Readme

---
+++ README.md
@@ -0,0 +1,26 @@
+# NASA App Mobile (NASA Open APIs)
+
+
+## Requirements:
+
+JDK 17.0.11
+Android API 31
+Ionic 7.2.0
+Node v20.14.0
+Angular v18.0.0
+
+
+## To run the project:
+
+    npm install
+
+    ionic server
+
+
+## To Build on Android:
+
+    ionic build
+
+    npx cap sync android
+
+    ionic cap build android --configuration development
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,17 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonModal } from '@ionic/angular'; // Importando IonModal
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-
 export class HomePage {
+  @ViewChild('apodModal') apodModal!: IonModal;
+  @ViewChild('neowsModal') neowsModal!: IonModal;
+  @ViewChild('donkiModal') donkiModal!: IonModal;
+  @ViewChild('earthModal') earthModal!: IonModal;
+  @ViewChild('eonetModal') eonethModal!: IonModal;
+  @ViewChild('epictModal') epicModal!: IonModal;
   presentingElement: Element | null = null;
 
   constructor() {}
 
   ngOnInit() {
     this.presentingElement = document.querySelector('.ion-page');
+  }
+
+  dismissApodModal() {
+    this.apodModal.dismiss();
+  }
+
+  dismissNeowsModal() {
+    this.neowsModal.dismiss();
+  }
+
+  dismissDonkiModal() {
+    this.donkiModal.dismiss();
+  }
+
+  dismissEarthModal() {
+    this.earthModal.dismiss();
+  }
+
+  dismissEonetModal() {
+    this.eonethModal.dismiss();
+  }
+
+  dismissEpicModal() {
+    this.epicModal.dismiss();
   }
 }
