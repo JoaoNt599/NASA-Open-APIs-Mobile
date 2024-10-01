92374991c980116fa6607d4bfc78a5322990597f
João Vieira <netojoao599@gmail.com>
2024-10-01 15:38:15 -0300

    Nav-menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
+
 export class AppComponent {
-  constructor() {}
+
+  constructor(private menu: MenuController) {}
+
+  closeMenu() {
+    this.menu.close('first');
+  }
 }
