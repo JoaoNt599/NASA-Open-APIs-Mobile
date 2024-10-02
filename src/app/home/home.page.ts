01ff68ea8904309ee8fd7e34e0f9674c34fb585f
João Vieira <netojoao599@gmail.com>
2024-10-02 00:11:22 -0300

    New design template

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -15,6 +15,15 @@ export class HomePage {
   @ViewChild('epictModal') epicModal!: IonModal;
   @ViewChild('exoplanetModal') exoplanetModal!: IonModal;
   @ViewChild('osdrModal') osdrModal!: IonModal;
+  @ViewChild('insightModal') insightModal!: IonModal;
+  @ViewChild('mrpModal') mrpModal!: IonModal;
+  @ViewChild('nivlModal') nivlModal!: IonModal;
+  @ViewChild('ttModal') ttModal!: IonModal;
+  @ViewChild('sscModal') sscModal!: IonModal;
+  @ViewChild('ssdModal') ssdModal!: IonModal;
+  @ViewChild('techportModal') techportModal!: IonModal;
+  @ViewChild('tleapiModal') tleapiModal!: IonModal;
+  @ViewChild('wmtsModal') wmtsModal!: IonModal;
   presentingElement: Element | null = null;
 
   constructor() {}
@@ -53,5 +62,41 @@ export class HomePage {
 
   dismissOsdrModal() {
     this.osdrModal.dismiss();
+  }
+
+  dismissInsightModal() {
+    this.insightModal.dismiss();
+  }
+
+  dismissMrpModal() {
+    this.mrpModal.dismiss();
+  }
+
+  dismissNivlModal() {
+    this.nivlModal.dismiss();
+  }
+
+  dismissTtModal() {
+    this.ttModal.dismiss();
+  }
+
+  dismissSscModal() {
+    this.sscModal.dismiss();
+  }
+
+  dismissSsdModal() {
+    this.ssdModal.dismiss();
+  }
+
+  dismissTechportModal() {
+    this.techportModal.dismiss();
+  }
+
+  dismissTleapiModal() {
+    this.tleapiModal.dismiss();
+  }
+  
+  dismissWmtsModal() {
+    this.wmtsModal.dismiss();
   }
 }
